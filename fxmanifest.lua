9a49617b10dad460d7e483a6af32d6951764f284
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-21 18:32:58 +0700

    Add fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+--// FX Information \\--
+fx_version   'adamant'
+use_fxv2_oal 'no'
+lua54        'no'
+game         'gta5'
+
+--// Resource Information \\--
+name         'NKCore Event'
+author       'Benjamin4k'
+version      '1.0.0'
+repository   'https://github.com/naka-studios/NKCore'
+description  'NKCore Game Event'
+
+--// Manifest \\--
+dependencies {
+    '/server:5562'
+}
+
+shared_scripts {
+	'class/common.lua',
+    'class/debug.lua',
+    'print.lua'
+}
+
+server_scripts {
+    'main.lua',
+    'class/identifier.lua',
+    'event/core.lua',
+    'function/drop.lua',
+    'function/webhook.lua',
+    'event/main.lua',
+    'loop/main.lua'
+}
